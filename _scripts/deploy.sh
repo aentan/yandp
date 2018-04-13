4cbb8973f4b4bef4c2245e10ccd7b14fb166cbe0
Aen <hello@aentan.com>
2018-04-13 17:35:31 +0800

    temp cname

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -27,7 +27,7 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public
-echo youngandpowerful.sg >> CNAME
+echo yandp.aenism.com >> CNAME
 git add --all
 git commit -m "Publishing to gh-pages (deploy.sh)"
 
