e4c32ba06ff5915ec711da70dfb897b7c8c77c71
Aen <hello@aentan.com>
2018-04-27 17:00:01 +0800

    wrong dist id

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,13 +4,13 @@ set -e
 # Run this in your Hugo blog repo directory
 
 # These are mine. They won't work for you :)
-DISTRIBUTION_ID=E26RWIBAFNRGCR
+DISTRIBUTION_ID=E1J7ZV6LIKLO6N
 BUCKET_NAME=youngandpowerful.sg
-PROFILE=lauf # or `default` if you don't use profiles
+PROFILE=default # or `default` if you don't use profiles
 
 hugo -v
 
-# Copy over pages - not static js/img/css/downloads
+# Copy over pages
 aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$BUCKET_NAME
 
 # Ensure static files are set to cache forever - cache for a month --cache-control "max-age=2592000"
